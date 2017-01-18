73eb93fb513bad557fc68b782314650c25e7bf4f
yusko43 <ecemilgun97@gmail.com>
2017-01-18 03:43:49 +0200

    Add files via upload

---
+++ ReservationSystem.h
@@ -0,0 +1,29 @@
+/*
+ * Ecem ilg?n
+ * 21502157
+ */
+
+#include "Flight.h"
+
+class ReservationSystem {
+    public:
+        ReservationSystem();
+        ~ReservationSystem();
+
+        void addFlight( const int flightNo, const int rowNo, const int seatNo );
+        void cancelFlight( const int flightNo );
+
+        void showAllFlights();
+        void showFlight( const int flightNo );
+
+        int makeReservation( const int flightNo, const int numPassengers,
+                             const int *seatRow, const char *seatCol );
+
+        void cancelReservation( const int resCode);
+        void showReservation( const int resCode );
+
+    private:
+        int counter;
+        int  noOfFlights;
+        Flight *system;
+};
